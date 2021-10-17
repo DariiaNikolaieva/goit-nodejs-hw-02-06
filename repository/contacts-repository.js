22f722f3c1f2b3e47dc9d652370687ea804be664
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2021-10-17 12:58:46 +0300

    filter

--- repository/contacts-repository.js
+++ repository/contacts-repository.js
@@ -1,48 +1,67 @@
-const Contact = require('../model/contact-model')
+const Contact = require("../model/contact-model");
 
 const listContacts = async (userId, query) => {
   // return await Contact.find({owner: userId}).populate({path: "owner", select: "name email"})
-const {sortBy, sortByDesc, favorite, limit = 5, offset = 0} = query;
-if (favorite !== null) {
-  searchOptions.favorite = favorite;
-}
-const searchOption = {owner: userId};
-const results = await Contact.paginate(searchOption, {
-  limit, 
-  offset, 
-  sort: {
-    ...(sortBy ? { [`${sortBy}`]: 1 } : {}),
-    ...(sortByDesc ? { [`${sortByDesc}`]: -1 } : {}),
-  },
-  populate: {
-    path: "owner", 
-    select: "name email"
+  const { 
+    sortBy, 
+    sortByDesc, 
+    filter,
+    favorite = null, 
+    limit = 5, 
+    offset = 0 
+  } = query;
+  const searchOptions = { owner: userId };
+  if (favorite !== null) {
+    // console.log(typeof favorite);
+    searchOptions.favorite = favorite;
   }
-})
-const {docs: contacts} = results;
-delete results.docs
-return {...results, contacts};
-}
+  const results = await Contact.paginate(searchOptions, {
+    limit,
+    offset,
+    sort: {
+      ...(sortBy ? { [`${sortBy}`]: 1 } : {}),
+      ...(sortByDesc ? { [`${sortByDesc}`]: -1 } : {}),
+    },
+    select: filter ? filter.split("|").join(" ") : "", 
+    populate: {
+      path: "owner",
+      select: "name email",
+    },
+  });
+  const { docs: contacts } = results;
+  delete results.docs;
+  return { ...results, contacts };
+};
 
 const getContactById = async (contactId, userId) => {
-  const result = await Contact.findOne({_id: contactId, owner: userId}).populate({path: "owner", select: "name email"})
-  return result
-}
+  const result = await Contact.findOne({
+    _id: contactId,
+    owner: userId,
+  }).populate({ path: "owner", select: "name email" });
+  return result;
+};
 
 const removeContact = async (contactId, userId) => {
-  const result = await Contact.findOneAndRemove({_id: contactId, owner: userId})
-  return result
-}
+  const result = await Contact.findOneAndRemove({
+    _id: contactId,
+    owner: userId,
+  });
+  return result;
+};
 
 const addContact = async (body) => {
-  const result = await Contact.create(body)
-  return result
-}
+  const result = await Contact.create(body);
+  return result;
+};
 
 const updateContact = async (contactId, body, userId) => {
-  const result = await Contact.findOneAndUpdate({_id: contactId, owner: userId}, { ...body }, { new: true })
-  return result
-}
+  const result = await Contact.findOneAndUpdate(
+    { _id: contactId, owner: userId },
+    { ...body },
+    { new: true }
+  );
+  return result;
+};
 
 module.exports = {
   listContacts,
@@ -50,4 +69,4 @@ module.exports = {
   removeContact,
   addContact,
   updateContact,
-}
+};
