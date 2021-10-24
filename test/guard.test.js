e95bf98a4ff1c228c2a4c84310a5d146a740ee9f
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2021-10-24 16:07:24 +0300

    feat: guard test

--- test/guard.test.js
+++ test/guard.test.js
@@ -1,25 +1,47 @@
-const guard = require('../helpers/guard');
-const passport = require("../config/passport");
-const { HttpCode } = require("../config/constants");
+const guard = require('../helpers/guard')
+const passport = require('passport')
+const { HttpCode } = require('../config/constants')
 
 describe('Unit test guard helper', () => {
-    const user = {token: '111222333'}
-    const req = { get: user }
-    const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn((data) => data),
-    }
-    const next = jest.fn();
+    const user = { token: '111222333' }
+    let req, res, next
 
     beforeEach(() => {
+        req = { get: jest.fn((header) => `Bearer ${user.token}`), user }
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn((data) => data),
+        }
+        next = jest.fn()
     })
 
     it('User exists', async () => {
+        passport.authenticate = jest.fn(
+            (strategy, options, cb) => (req, res, next) => cb(null, user),
+        )
+        await guard(req, res, next)
+        expect(req.get).toHaveBeenCalled()
+        expect(next).toHaveBeenCalled()
     })
 
-    it('User exists with wrong token', async () => {
+    it('User exists with the wrong token', async () => {
+        passport.authenticate = jest.fn(
+            (strategy, options, cb) => (req, res, next) =>
+                cb(null, { token: '123456' }),
+        )
+        await guard(req, res, next)
+        expect(req.get).toHaveBeenCalled()
+        expect(res.status).toHaveBeenCalledWith(HttpCode.UNAUTHORIZED)
+        expect(res.json).toHaveBeenCalled()
     })
 
     it('User does not exist', async () => {
+        passport.authenticate = jest.fn(
+            (strategy, options, cb) => (req, res, next) => cb(null, false),
+        )
+        await guard(req, res, next)
+        expect(req.get).toHaveBeenCalled()
+        expect(res.status).toHaveBeenCalledWith(HttpCode.UNAUTHORIZED)
+        expect(res.json).toHaveBeenCalled()
     })
-}
+})
