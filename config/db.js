4b0ba015451de61e9b119b5d0ca919177951647c
Dariia Nikolaieva <74855191+DariiaNikolaieva@users.noreply.github.com>
2021-10-24 16:26:05 +0300

    fix

--- config/db.js
+++ config/db.js
@@ -1,25 +1,32 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+let uri;
 
-const uri = process.env.URI_DB;
+if (process.env.NODE_ENV == 'test') {
+    uri = process.env.URI_DB_TEST;
+} else {
+    uri = process.env.URI_DB;
+}
 
 const db = mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 })
 
-mongoose.connection.on('connected', () => {
-  console.log('Database connection successful')
-});
+if (process.env.NODE_ENV == 'test') {
+    mongoose.connection.on('connected', () => {
+        console.log('Database connection successful')
+    });
 
-mongoose.connection.on('error', (err) => {
-  console.log(`Database connection error ${err.message}`)
-});
+    mongoose.connection.on('error', (err) => {
+        console.log(`Database connection error ${err.message}`)
+    });
+}
 
 process.on('SIGINT', async () => {
-  await mongoose.connection.close();
-  console.log('Connection to DB closed');
-  process.exit(1);
+    await mongoose.connection.close();
+    console.log('Connection to DB closed');
+    process.exit(1);
 })
 
 module.exports = db;
